d49f4d8c88bceb8ac6319b48dcdac2d9e40ef349
Edward Akande <2022akande@gmail.com>
2021-08-18 11:46:02 -0400

    2nd commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Arbalest: The Productivity App</h1>
+        </header>
+    );
+};
+
+export default Header;
